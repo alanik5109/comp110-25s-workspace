dcf5b617640f075e8e424395bd76293782913e93
alanik5109 <alanik@unc.edu>
2025-01-14 18:04:49 -0500

    Final ish?

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,3 +1,7 @@
+"""My first exercise in COMP110!"""
+__author__ = "730547147"
 def greet(name: str) -> str:
     """A welcoming first function definition."""
     return "Hello, " + name + "!"
+if __name__ == "__main__":
+    print(greet(name=input("What is your name? ")))
